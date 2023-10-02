6acdfb58e3555723d4fa4484f44b9ae9adc45dba
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2023-10-02 23:00:36 +0530

    added port

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 var cors = require("cors");
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 8000;
 const mongoDB= require('./database/db');
 
 mongoDB();
