09e82b8e8d3dc1783371123e5cccf6dcc1e8b501
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2023-10-02 19:20:59 +0530

    new commit

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use('/api/', require('./routes/Createuser'));
 if (process.env.NODE_ENV === 'production') 
  {
     //Set static folder up in production
-    app.use(express.static('frontend/build'));
+    app.use(express.static('newclient/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'newclient', 'build','index.html')));
   }
 
 
